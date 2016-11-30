21b1fbfb58ab870933109e0bde72084305fd0f24
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-11-30 19:37:35 +0100

    Test mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+var mongo = require('mongodb').MongoClient;
 var app = require('express')();
+var url = "mongodb://jorisboschmans:ITrules4565@ds029635.mlab.com:29635/jorisboschmans-mydb";
+var col = "paybackfriendsserver";
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res){
-  res.send("Hello world");
+  if (err) throw err;
+  db.collection(col).find().toArray(function(err, docs){
+    if (err) throw err;
+    res.send(JSON.stringify(docs));
+  });
 });
 
 app.listen(process.env.PORT || 3000, function(){
